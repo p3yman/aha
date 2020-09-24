76a1c6d61e9905bd4937c5c7c3015183a64cede3
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-09-24 18:02:19 +0300

    Init new data.json file

--- cli.js
+++ cli.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 
+const jsonfile = require('jsonfile');
+
+const DATA_FILE = 'data.json';
+
 module.exports = function() {
   console.log('Welcome to the CLI');
+
+  let data = { rows: [] };
+
+  try {
+    data = jsonfile.readFileSync(file);
+  } catch (err) {
+    jsonfile.writeFileSync(DATA_FILE, data, { spaces: 2 });
+  }
+
+  console.log(data);
+
 };
