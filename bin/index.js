916e423e628bf46aa5e853a7442c2353e08b4e30
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-09-24 20:30:15 +0300

    Simplify running comments

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,3 @@
 #!/usr/bin/env node
 
-const command = require('../src');
-
-command();
+require('../src');
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-const jsonfile = require('jsonfile');
 const program = require('commander');
 
 program.version('0.1.0');
@@ -9,21 +8,3 @@ require('./commands/list');
 
 // Parse Args
 program.parse(process.argv);
-
-const DATA_FILE = 'data.json';
-
-module.exports = function() {
-  console.log('Welcome to the CLI');
-
-  let data = { rows: [] };
-
-  // Read file and create new one if it doesn't exist.
-  try {
-    data = jsonfile.readFileSync(file);
-  } catch (err) {
-    jsonfile.writeFileSync(DATA_FILE, data, { spaces: 2 });
-  }
-
-  console.log(data);
-
-};
