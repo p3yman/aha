01da8e27a0de617eb979cf9c9e20e90092c83b7e
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-09-24 19:16:27 +0300

    Test showing list table

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,9 +1,37 @@
 #!/usr/bin/env node
 const program = require('commander');
+const chalk = require('chalk');
+const { table } = require('table');
 
 program
   .description('List all your aha moments.')
   .command('list')
   .action((options) => {
-    console.log('List command called');
+    const config = {
+      columns: {
+        0: {
+            alignment: 'left',
+            width    : 3,
+        },
+        1: {
+            alignment: 'left',
+            width    : 30,
+        },
+        2: {
+            alignment: 'left',
+        },
+        3: {
+          alignment: 'left',
+        },
+      },
+    };
+
+    const data = [
+      [chalk.blue.bold('ID'), chalk.blue.bold('Title'), chalk.blue.bold('Status'), chalk.blue.bold('Created at')],
+      [1, 'The title', chalk.black.bgGreen.bold(' Done '), 'Yesterday'],
+      [999, 'The title', 'Pending', 'Yesterday'],
+    ];
+
+    const output = table(data, config);
+    console.log(output);
   });
