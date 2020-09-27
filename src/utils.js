e5f19acbdb446addd4c05a473b9024cc312c85fd
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-09-27 17:31:08 +0300

    Add show error and success helpers

--- src/utils.js
+++ src/utils.js
@@ -5,8 +5,40 @@ const chalk = require('chalk');
  * 
  * @param {string} note 
  */
-const showHelpNote = (note) => console.log( chalk.black.bgYellow(' NOTE ') + chalk.white.bgGray(` ${note} `) );
+const showHelpNote = (note) => console.log( '   ' + chalk.black.bgYellow(' NOTE ') + chalk.white.bgGray(` ${note} `) );
+
+/**
+ * Show error message
+ * 
+ * @param {string} msg 
+ */
+const showError = (msg) => console.log( '\n   ' + chalk.white.bgBlack(' ERROR ') + chalk.black.bgRed(` ${msg} `) + '\n');
+
+/**
+ * Show success message
+ * 
+ * @param {string} msg 
+ */
+const showSuccess = (msg) => console.log( '\n   ' + chalk.black.bgGreen(' SUCCESS ') + chalk.white.bgGray(` ${msg} `) + '\n');
+
+/**
+ * Show proper label for status.
+ * 
+ * @param {string} status 
+ */
+const statusLabel = (status) => {
+  if (status === 'done') {
+    return chalk.black.bgGreen(' Done ');
+  } else if (status === 'pending') {
+    return chalk.black.bgYellow(' Pending ');
+  } else if (status === 'cancelled') {
+    return chalk.black.bgRed(' Cancelled ');
+  }
+};
 
 module.exports = {
   showHelpNote,
+  showError,
+  showSuccess,
+  statusLabel,
 }
