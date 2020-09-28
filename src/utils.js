46b0ad3377494b2b35a0b347448217160969dbd2
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-09-28 08:51:24 +0300

    add showEmptyMessage helper

--- src/utils.js
+++ src/utils.js
@@ -5,21 +5,26 @@ const chalk = require('chalk');
  * 
  * @param {string} note 
  */
-const showHelpNote = (note) => console.log( '   ' + chalk.black.bgYellow(' NOTE ') + chalk.white.bgGray(` ${note} `) );
+const showHelpNote = (note) => console.log( '   ' + chalk.black.bgYellow(' NOTE ') + chalk(` ${note} `) );
 
 /**
  * Show error message
  * 
  * @param {string} msg 
  */
-const showError = (msg) => console.log( '\n   ' + chalk.white.bgBlack(' ERROR ') + chalk.black.bgRed(` ${msg} `) + '\n');
+const showError = (msg) => console.log( '\n   ' + chalk.white.bgRed(' ERROR ') + chalk.bold(` ${msg} `) + '\n');
 
 /**
  * Show success message
  * 
  * @param {string} msg 
  */
-const showSuccess = (msg) => console.log( '\n   ' + chalk.black.bgGreen(' SUCCESS ') + chalk.white.bgGray(` ${msg} `) + '\n');
+const showSuccess = (msg) => console.log( '\n   ' + chalk.black.bgGreen(' SUCCESS ') + chalk.bold(` ${msg} `) + '\n');
+
+/**
+ * Show message if list is empty
+ */
+const showEmptyMessage = () =>  console.log( '\n   ' + chalk.yellow(' You don\'t have any aha moments for now. You can add one using `' + chalk.bold('aha add "title"') + '` command.') + '\n');
 
 /**
  * Show proper label for status.
@@ -28,11 +33,11 @@ const showSuccess = (msg) => console.log( '\n   ' + chalk.black.bgGreen(' SUCCES
  */
 const statusLabel = (status) => {
   if (status === 'done') {
-    return chalk.black.bgGreen(' Done ');
+    return chalk.green('Done');
   } else if (status === 'pending') {
-    return chalk.black.bgYellow(' Pending ');
+    return chalk.yellow('Pending');
   } else if (status === 'cancelled') {
-    return chalk.black.bgRed(' Cancelled ');
+    return chalk.red('Cancelled');
   }
 };
 
@@ -41,4 +46,5 @@ module.exports = {
   showError,
   showSuccess,
   statusLabel,
+  showEmptyMessage,
 }
